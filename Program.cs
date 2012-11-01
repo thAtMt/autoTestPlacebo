2422a854769d73d07add54e8945ba3df483af017
talh <th@mythings.com>
2012-11-01 14:10:18 +0200

    First commit of test project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace autoTestPlacebo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Was cahnged to foo bar");
+            Console.ReadKey();
+        }
+    }
+}
